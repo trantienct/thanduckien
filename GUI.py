6a619148cc898ded696325d7d4eeec7112eae6ae
CT_TienTV <tientv@mcrew-tech.com>
2023-02-13 21:40:37 +0700

    Update GUI with some elements

--- GUI.py
+++ GUI.py
@@ -11,4 +11,7 @@ lblTitle.grid(column=0, row=0, columnspan=3)
 btnView = Button(root, text='View', command=account1.view)
 btnView.grid(column=0, row=1)
 
+btnDeposit = Button(root, text='Deposit', command=account1.deposit)
+btnDeposit.grid(column=1, row=1)
+
 root.mainloop()
