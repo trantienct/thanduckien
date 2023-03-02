8832dcc4f8dd052cdda4686d51025faa2979c295
CT_TienTV <tientv@mcrew-tech.com>
2023-03-02 21:04:17 +0700

    First GUI for Login Page

---
+++ GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+from login import *
+
+root = Tk()
+root.title('Library Management System')
+root.geometry('400x400')
+loginPage(root)
+root.mainloop()
