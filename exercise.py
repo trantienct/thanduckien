fe76eec95bf655cb2117c358e1b76fb661b2f5e2
CT_TienTV <tientv@mcrew-tech.com>
2023-03-21 21:49:23 +0700

    Add exercise 21032023

--- exercise.py
+++ exercise.py
@@ -1,2 +1,9 @@
-# BTVN 13032023
-# Thuc hien chuc nang Update User va load lai data vao TreeView
+# BTVN 21032023
+# Thiet ke function validateData cho Add user form
+# Kiểm tra các trường hợp sau trước khi insert
+# TH1: Username và password chưa được nhập
+# TH2: Username có số ký tự nhỏ hơn 3 hoặc lớn hơn 10
+# TH3: Password có số ký tự nhỏ hơn 3 hoặc lớn hơn 10
+# TH4: Password không có ký tự @
+# TH5: Role chưa được chọn
+# HIển thị message tương ứng từng trường hợp
