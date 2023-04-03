47918b17224f427f84cb3d4a8f5467f00f076d52
CT_TienTV <tientv@mcrew-tech.com>
2023-04-03 08:31:08 +0700

    Update exercise 27-03-2023

--- exercise.py
+++ exercise.py
@@ -2,6 +2,6 @@
 #BT1: Làm lại form Add User
 #Kiểm tra dữ liệu khi Administrator tạo người dùng mới
 # - Username: Không rỗng, tối đa 30 ký tự, không có các ký tự đặc biệt như @, #, $, %
-# - Password: Không rỗng, tối thiểu 8 ký tự, không có các ký tự đặc biệt như @, #, $, %
+# - Password: Không rỗng, tối thiểu 8 ký tự, chứa 1 trong ký tự đặc biệt: @, #, $, %
 # - Role: Không rỗng
 # - Khi kiểm tra trong DB, username phải chưa tồn tại
