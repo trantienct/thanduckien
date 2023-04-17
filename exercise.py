1296c67fd052f1dd52e38b9afa626e2d840a99ff
CT_TienTV <tientv@mcrew-tech.com>
2023-04-17 21:47:28 +0700

    Add excercise 17/4/2023

--- exercise.py
+++ exercise.py
@@ -1,6 +1,11 @@
-#BTVN 3/4/2023
-# Ref Link https://www.w3schools.com/python/python_regex.asp
-
-# Check password
-# Check email is valid: contains @, .
-
+#Exercise 17/4/2023:
+# BT1: Hoan thanh them 1 cuon sach
+# - Validate data
+# - Check if book name exists
+# - If not, insert new book
+# - Else, show Messagebox
+# BT2:
+# Add column book_image to Book Table: book_image BLOB
+# Check mail and follow instruction
+# - Save to DB
+# - Load into tkinter
