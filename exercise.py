6a4b90541df699bad409ded5dd015cf385d8cb92
CT_TienTV <tientv@tora-tech.com>
2023-05-29 21:40:08 +0700

    BTVN

--- exercise.py
+++ exercise.py
@@ -1,3 +1,5 @@
-#Exercise 15/5/2023:
-# BT1: Thêm Label để hiển thị image sau khi upload ở trang Addbook
-# BT2: Khi bấm xem chi tiết 1 cuốn sách ở View List Book để hiển thị qua Toplevel Detail, Hãy lấy image từ DB và hiển thị lên màn hình
+#Exercise 29/5/2023:
+# BTVN: Di chuyển theo flow sau:
+# View book list -> view book info -> Click update (Ẩn cửa sổ book info, hiện ra Update Form)
+# Update data from Form.
+# After updating, return to book list and reload data
