443b970252f730b4ebbae914a0a39cc121559c37
CT_TienTV <tientv@tora-tech.com>
2023-06-12 21:39:58 +0700

    Add exercise 12/6/2023

--- exercise.py
+++ exercise.py
@@ -1,5 +1,3 @@
-#Exercise 29/5/2023:
-# BTVN: Di chuyển theo flow sau:
-# View book list -> view book info -> Click update (Ẩn cửa sổ book info, hiện ra Update Form)
-# Update data from Form.
-# After updating, return to book list and reload data
+#Exercise 12/6/2023:
+# Add validation rule when borrowing a book
+# Reset book list after borrowing a book
